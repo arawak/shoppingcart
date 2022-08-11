4d51c192994f488bc700c1bf1768dc0d74a231a4
Sean Mitchell <sean@mitchwood.com>
2022-08-10 20:25:38 -0400

    rename ugly class

---
+++ src/test/java/com/mitchwood/cart/ShoppingCartTests.java
@@ -0,0 +1,13 @@
+package com.mitchwood.cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
